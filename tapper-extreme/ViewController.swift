18e52274f63b70377a2be0751e768052b63d6567
Cody Condon <codycondon50@gmail.com>
2016-04-10 15:58:59 -0400

    onplaybtnpressed working

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties 
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    // Outlets
     @IBOutlet weak var logoImg: UIImageView!
     @IBOutlet weak var howManyTapsTxt: UITextField!
     @IBOutlet weak var playBtn: UIButton!
@@ -17,7 +22,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapBtn: UIButton!
     @IBOutlet weak var tapsLbl: UILabel!
     
+    @IBAction func onCoinTapped(sender: UIButton! ) {
+        
+    }
     
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+       
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0
+            
+            tapsLbl.text = "\(currentTaps) Taps"
+        }
+        
+    }
     
 
 }
